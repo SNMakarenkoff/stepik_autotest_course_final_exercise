5034f32912da116b05a3a0fd1c7df2b465a43a22
Sergey <snmakarenkoff@gmail.com>
2022-02-16 12:14:24 +0300

    Добавлены локаторы для login_page, описаны проверки на корректность url и отображение форм авторизации и регистрации

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+form .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert self.browser.current_url('login'), 'URL не содержит подстроку "login"'
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Форма авторизации не найдена'
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'Форма регистрации не найдена'

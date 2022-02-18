4cd541ffdb9b0aacb6bc843310f45dff7b3f3565
Sergey <snmakarenkoff@gmail.com>
2022-02-18 14:34:46 +0300

    Добавление теста для Product Page c параметризцией

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+class ProductPageLocators():
+    ADD_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    BOOK_NAME = (By.CSS_SELECTOR, '.product_main h1')
+    BOOK_PRICE = (By.CSS_SELECTOR, '.price_color:nth-child(2)')
+    BOOK_NAME_CHECK = (By.CSS_SELECTOR, '.alertinner strong')
+    BOOK_PRICE_CHECK = (By.CSS_SELECTOR, '.alertinner p strong')
---
+++ pages/product_page.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def added_to_basket(self):
+        link = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
+        link.click()
+
+    def should_be_same_book_name(self):
+        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME)
+        book_name_check = self.browser.find_element(*ProductPageLocators.BOOK_NAME_CHECK)
+        assert book_name.text == book_name_check.text, 'Название книги в корзине не совпадает с выбранной'
+
+    def should_be_same_price(self):
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE)
+        book_price_check = self.browser.find_element(*ProductPageLocators.BOOK_PRICE_CHECK)
+        assert book_price.text == book_price_check.text, 'Цена книги в корзине не совпадает с выбранной'
